cfe388fdb80b5c779e081e47a7fa4e9090eecafc
Wael Zantour <waelzantour1@gmail.com>
2024-10-08 23:54:10 +0200

    Include the OpenApi documentation

---
+++ src/main/java/com/pmt/project_management/config/OpenApiConfig.java
@@ -0,0 +1,45 @@
+package com.pmt.project_management.config;
+
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "Project Management Tool",
+                        email = "mpmt@contact.com"
+                ),
+                description = "OpenApi documentation for spring security",
+                title = "Open specification - Medhead Consortium",
+                version = "1.0",
+                termsOfService = "terms of service"
+        ),        servers = {
+        @Server(
+                description = "Local ENV",
+                url = "http://localhost:8088"
+        ),
+},
+        security = {
+                @SecurityRequirement(
+                        name = "bearerAuth"
+                )
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT auth description",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+
+)
+public class OpenApiConfig {
+}
